6868dbcd913cbcd66e03ee4579ad54d0371a196f
mombol <mombol@163.com>
2016-06-02 15:38:43 +0800

    add transfer object pattern

---
+++ transfer-object-pattern/src/TransferObjectPatternDemo.java
@@ -0,0 +1,23 @@
+public class TransferObjectPatternDemo
+{
+    public static void main(String[] args)
+    {
+        StudentBO studentBusinessObject = new StudentBO();
+
+        // print all students
+        for (StudentVO student : studentBusinessObject.getAllStudents()) {
+            System.out.println("Student: [RollNo : "
+                    + student.getRollNo() + ", Name : " + student.getName() + " ]");
+        }
+
+        // update student info
+        StudentVO student = studentBusinessObject.getAllStudents().get(0);
+        student.setName("Michael");
+        studentBusinessObject.updateStudent(student);
+
+        // get student info
+        studentBusinessObject.getStudent(0);
+        System.out.println("Student: [RollNo : "
+                + student.getRollNo() + ", Name : " + student.getName() + " ]");
+    }
+}
